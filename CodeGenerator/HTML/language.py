2f1f0ca55d0ead3e9cc81866e3df9eb18d6b90c1
Bernardo Fanti <bfanti@peaksware.com>
2015-07-21 21:37:03 -0600

    Add bitfields to HTML generator

--- CodeGenerator/HTML/language.py
+++ CodeGenerator/HTML/language.py
@@ -12,14 +12,17 @@ def reflection(msg):
 def accessors(msg):
     return ""
 
-def createFieldInfoRow(field, messageName):
-    name = str(field["Name"]) if "Name" in field else "n/a"
-    fieldtype = str(field["Type"]) if "Type" in field else "n/a"
+def createFieldInfoRow(field):
+    name = str(field["Name"]) if "Name" in field else "ERROR"
+    fieldtype = str(field["Type"]) if "Type" in field else "ERROR"
     count = str(field["Count"]) if "Count" in field else "1"
+
     units = str(field["Units"]) if "Units" in field else "n/a"
 
     if "Enum" in field:
         units = str(field["Enum"])
+    elif "Bitfields" in field:
+        units = "Bitfield"
 
     scale = str(field["Scale"]) if "Scale" in field else "n/a"
     offset = str(field["Offset"]) if "Offset" in field else "n/a"
@@ -38,20 +41,47 @@ def createFieldInfoRow(field, messageName):
     "
     return html
 
+def createBitfieldInfoRow(bitfield):
+    name = "&nbsp&nbsp&nbsp&nbsp" + str(bitfield["Name"]) if "Name" in bitfield else "ERROR"
+    bitfieldtype = str(bitfield["NumBits"]) + " bits" if "NumBits" in bitfield else "ERROR"
+    count = str(bitfield["Count"]) if "Count" in bitfield else "1"
+    units = str(bitfield["Units"]) if "Units" in bitfield else "n/a"
+
+    if "Enum" in bitfield:
+        units = str(bitfield["Enum"])
+
+    scale = str(bitfield["Scale"]) if "Scale" in bitfield else "n/a"
+    offset = str(bitfield["Offset"]) if "Offset" in bitfield else "n/a"
+    description = str(bitfield["Description"]) if "Description" in bitfield else "n/a"
+
+    html = "\
+        <tr>\
+            <td>" + name + "</td>\
+            <td>" + bitfieldtype + "</td>\
+            <td>" + count + "</td>\
+            <td>" + units + "</td>\
+            <td>" + scale + "</td>\
+            <td>" + offset + "</td>\
+            <td>" + description + "</td>\
+        </tr>\
+    "
+    return html
+
+
 def initCode(msg):
     ret = []
 
     for field in msg["Fields"]:
-        fieldInfoRow = createFieldInfoRow(field, msg["Name"])
+        fieldInfoRow = createFieldInfoRow(field)
         
         if fieldInfoRow:
             ret.append(fieldInfoRow)
         
-        #if "Bitfields" in field:
-        #    for bits in field["Bitfields"]:
-        #        bits = createBitfieldInfo(field, bits, msg["Name"])
-        #        if bits:    
-        #            ret.append(bits)
+        if "Bitfields" in field:
+            for bits in field["Bitfields"]:
+                bits_html = createBitfieldInfoRow(bits)
+                if bits:    
+                    ret.append(bits_html)
 
     return ret
 
