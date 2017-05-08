382e4e608dece0b433187d421ac79886f2f37028
mgazic <mgazic@github.com>
2017-05-08 13:59:56 -0400

    Refactor out TreeWidget to common location to allow use by multiple applications.

--- MsgApp/MsgGui.py
+++ MsgApp/MsgGui.py
@@ -4,6 +4,184 @@ from PyQt5 import QtGui, QtWidgets, QtCore, QtNetwork
 
 from MsgApp import *
 
+# tree widget item with support for sorting by a column
+class TreeWidgetItem(QtWidgets.QTreeWidgetItem):
+    def __init__(self, parent, stringList):
+        QtWidgets.QTreeWidgetItem.__init__(self, parent, stringList)
+
+    def __lt__(self, otherItem):
+        column = self.treeWidget().sortColumn()
+        try:
+            return float(self.text(column)) < float(otherItem.text(column))
+        except ValueError:
+            return self.text(column) < otherItem.text(column)
+
+# tree widget that we can copy-and-paste on
+class TreeWidget(QtWidgets.QTreeWidget):
+    def __init__(self):
+        super(TreeWidget, self).__init__() # what constructor parameters?!
+        self.setSelectionMode(self.ContiguousSelection)
+        self.setSelectionBehavior(self.SelectRows)
+
+    def keyPressEvent(self, keyEvent):
+        if keyEvent.matches(QtGui.QKeySequence.Copy):
+            self.copySelection()
+        else:
+            super(TreeWidget, self).keyPressEvent(keyEvent)
+
+    def copySelection(self):
+        selectedItems = self.selectedItems()
+        copiedText = ""
+        h = self.headerItem()
+        for col in range(h.columnCount()):
+            if col != 0:
+                copiedText += ", "
+            copiedText += h.text(col)
+        copiedText += "\n"
+        
+        for itemNumber in range(len(selectedItems)):
+            w = selectedItems[itemNumber]
+            for col in range(w.columnCount()):
+                if col != 0:
+                    copiedText += ", "
+                copiedText += w.text(col)
+            copiedText += "\n"
+        clipboard = QtWidgets.QApplication.clipboard()
+        clipboard.setText(copiedText)
+
+# tree widget that allows us to push msg data directly into the tree
+class MsgTreeWidget(TreeWidget):
+    MAX_ROWS = 1000
+    ROWS_TO_DELETE = 50
+    def __init__(self, msgClass, keyField=None, maxRows=1000, rowsToDelete=50):
+        super(MsgTreeWidget, self).__init__()
+        # add table header, one column for each message field
+        tableHeader = []
+        tableHeader.append("Time (ms)")
+        for fieldInfo in msgClass.fields:
+            tableHeader.append(fieldInfo.name)
+            for bitInfo in fieldInfo.bitfieldInfo:
+                tableHeader.append(bitInfo.name)
+        self.setHeaderLabels(tableHeader)
+        # configure the header so we can click on it to sort
+        self.header().setSectionsClickable(1)
+        self.header().setSortIndicatorShown(1)
+        # show sort indicator ascending on Time, if not sorting, because we append incoming messages
+        self.header().setSortIndicator(0, QtCore.Qt.AscendingOrder)
+        self.header().myTreeWidget = self
+        self.header().sectionClicked.connect(self.tableHeaderClicked)
+        
+        self.maxRows = maxRows
+        if rowsToDelete > maxRows:
+            rowsToDelete = maxRows
+        self.rowsToDelete = rowsToDelete
+
+        # key field for our message, that we display one row per value of
+        self.keyField = keyField
+
+        self.firstTimeDataAdded = 1
+
+    def addData(self, msg, autoscroll=1):
+        msgStringList = []
+        columnAlerts = []
+        try:
+            msgStringList.append(str(msg.hdr.GetTime()))
+        except AttributeError:
+            msgStringList.append("Unknown")
+        columnAlerts.append(0)
+        keyColumn = -1
+        columnCounter = 1
+        for fieldInfo in type(msg).fields:
+            if(fieldInfo.count == 1):
+                fieldValue = str(Messaging.get(msg, fieldInfo))
+                msgStringList.append(fieldValue)
+                columnAlerts.append(Messaging.getAlert(msg, fieldInfo))
+                if fieldInfo.name == self.keyField:
+                    keyValue = fieldValue
+                    keyColumn = columnCounter
+                columnCounter += 1
+                for bitInfo in fieldInfo.bitfieldInfo:
+                    fieldValue = str(Messaging.get(msg, bitInfo))
+                    msgStringList.append(fieldValue)
+                    columnAlerts.append(Messaging.getAlert(msg, bitInfo))
+                    if bitInfo.name == self.keyField:
+                        keyValue = fieldValue
+                        keyColumn = columnCounter
+                    columnCounter += 1
+            else:
+                columnText = ""
+                alert = 0
+                for i in range(0,fieldInfo.count):
+                    fieldValue = Messaging.get(msg, fieldInfo, i)
+                    # if the value is what is given when we go off the end of an array, break.
+                    if fieldInfo.type == "int" and fieldValue == "UNALLOCATED":
+                        break
+                    columnText += str(fieldValue)
+                    if Messaging.getAlert(msg, fieldInfo, i):
+                        alert = 1
+                    if(i<fieldInfo.count-1):
+                        columnText += ", "
+                msgStringList.append(columnText)
+                columnAlerts.append(alert)
+                columnCounter += 1
+
+        msgItem = TreeWidgetItem(None,msgStringList)
+        for column in range(0, len(columnAlerts)):
+            if columnAlerts[column]:
+                font = msgItem.font(column)
+                brush = msgItem.foreground(column)
+                font.setBold(1)
+                brush.setColor(QtCore.Qt.red)
+                msgItem.setFont(column, font)
+                msgItem.setForeground(column, brush)
+                msgItem.setBackground(column, brush)
+        if self.keyField != None and keyColumn >= 0:
+            # find row that has key field that matches ours
+            foundAndReplaced = 0
+            for i in range(0, self.topLevelItemCount()):
+                item = self.topLevelItem(i)
+                if item.text(keyColumn) == keyValue:
+                    foundAndReplaced = 1
+                    self.takeTopLevelItem(i)
+                    self.insertTopLevelItem(i, msgItem)
+            if not foundAndReplaced:
+                self.addTopLevelItem(msgItem)
+                self.sortItems(keyColumn, QtCore.Qt.AscendingOrder)
+        else:
+            self.addTopLevelItem(msgItem)
+            if(autoscroll):
+                self.scrollToItem(msgItem)
+            # when deleting, make a bunch of room so that if we're auto scrolling we have a bit of time before it
+            # shifts the data.  Otherwise the user can't read stuff if it's going by too fast.
+            if self.topLevelItemCount() > self.maxRows:
+                for i in range(0, self.rowsToDelete):
+                    self.takeTopLevelItem(0)
+                    
+        if self.firstTimeDataAdded:
+            count = 0
+            for fieldInfo in type(msg).fields:
+                self.resizeColumnToContents(count)
+                count += 1
+        self.firstTimeDataAdded = 0
+        
+    def tableHeaderClicked(self, column):
+        fieldName = self.headerItem().text(column)
+        if self.keyField == None or self.keyField != fieldName:
+            self.keyField = fieldName
+            self.header().setSortIndicator(column, QtCore.Qt.AscendingOrder)
+            self.sortItems(column, QtCore.Qt.AscendingOrder)
+            valueToRemove = None
+            for i in range(self.topLevelItemCount()-1, -1 ,-1):
+                item = self.topLevelItem(i)
+                if not valueToRemove == None and item.text(column) == valueToRemove:
+                    self.takeTopLevelItem(i)
+                else:
+                    valueToRemove = item.text(column)
+        else:
+            self.keyField = None
+            self.header().setSortIndicator(0, QtCore.Qt.AscendingOrder)
+
+
 class MsgGui(MsgApp, QtWidgets.QMainWindow):
     def __init__(self, name, argv, options, parent=None):
         # default to Network, unless we have a input filename that contains .txt
--- MsgInspector/MsgInspector.py
+++ MsgInspector/MsgInspector.py
@@ -12,70 +12,7 @@ import MsgGui
 
 from Messaging import Messaging
 
-class TreeWidgetItem(QtWidgets.QTreeWidgetItem):
-
-    def __init__(self, parent, stringList):
-        QtWidgets.QTreeWidgetItem.__init__(self, parent, stringList)
-
-    def __lt__(self, otherItem):
-        column = self.treeWidget().sortColumn()
-        try:
-            return float(self.text(column)) < float(otherItem.text(column))
-        except ValueError:
-            return self.text(column) < otherItem.text(column)
-
-class TreeWidget(QtWidgets.QTreeWidget):
-    def keyPressEvent(self, keyEvent):
-        if keyEvent.matches(QtGui.QKeySequence.Copy):
-            self.copySelection()
-        else:
-            super(TreeWidget, self).keyPressEvent(keyEvent)
-
-    def copySelection(self):
-        selectedItems = self.selectedItems()
-        copiedText = ""
-        h = self.headerItem()
-        for col in range(h.columnCount()):
-            if col != 0:
-                copiedText += ", "
-            copiedText += h.text(col)
-        copiedText += "\n"
-        
-        for itemNumber in range(len(selectedItems)):
-            w = selectedItems[itemNumber]
-            for col in range(w.columnCount()):
-                if col != 0:
-                    copiedText += ", "
-                copiedText += w.text(col)
-            copiedText += "\n"
-        clipboard = QtWidgets.QApplication.clipboard()
-        clipboard.setText(copiedText)
-
-    def tableHeaderClicked(self, column):
-        tree = self
-        header = tree.header()
-        fieldName = tree.headerItem().text(column)
-        msgName = tree.msgName
-        if not msgName in self.inspector.keyFields or self.inspector.keyFields[msgName] != fieldName:
-            print("sorting " + msgName + " on " + fieldName)
-            header.setSortIndicator(column, QtCore.Qt.AscendingOrder)
-            self.inspector.keyFields[msgName] = fieldName
-            tree.sortItems(column, QtCore.Qt.AscendingOrder)
-            valueToRemove = None
-            for i in range(tree.topLevelItemCount()-1, -1 ,-1):
-                item = tree.topLevelItem(i)
-                if not valueToRemove == None and item.text(column) == valueToRemove:
-                    tree.takeTopLevelItem(i)
-                else:
-                    valueToRemove = item.text(column)
-        else:
-            print("not sorting " + msgName)
-            del self.inspector.keyFields[msgName]
-            header.setSortIndicator(0, QtCore.Qt.AscendingOrder)
-
 class MsgInspector(MsgGui.MsgGui):
-    MAX_ROWS = 1000
-    ROWS_TO_DELETE = 50
     def __init__(self, argv, parent=None):
         MsgGui.MsgGui.__init__(self, "Message Inspector 0.1", argv, [], parent)
         
@@ -127,136 +64,36 @@ class MsgInspector(MsgGui.MsgGui):
 
     def ProcessMessage(self, msg):
         hdr = msg.hdr
-        id       = hex(hdr.GetMessageID())
-        msgName = msg.MsgName()
+        
+        # create identifying string for type and source of message
+        msgRoute = Messaging.MsgRoute(msg)
+        msgKey = msg.MsgName()
+        if len(msgRoute) > 0 and not(all ("0" == a for a in msgRoute)):
+            msgKey += " ("+"->".join(msgRoute)+")"
 
-        replaceMode = 0
         if self.allowedMessages:
-            if not msgName in self.allowedMessages:
+            if not msg.MsgName() in self.allowedMessages:
                 return
-        if msgName in self.keyFields:
-            replaceMode = 1
 
-        firstTime = 0
-        if(not(id in self.msgWidgets)):
-            firstTime = 1
+        if(not(msgKey in self.msgWidgets)):
             # create a new tree widget
-            msgWidget = TreeWidget()
-            msgWidget.inspector = self
+            try:
+                keyField = self.keyFields[msg.MsgName()]
+            except KeyError:
+                keyField = None
+            msgWidget = MsgGui.MsgTreeWidget(type(msg), keyField)
+
+            # connect to double-clicked signal to change scrolling globally
             msgWidget.itemDoubleClicked.connect(self.tableDataDoubleClicked)
-            msgWidget.msgName = msgName
-            msgWidget.setSelectionMode(msgWidget.ContiguousSelection)
-            msgWidget.setSelectionBehavior(msgWidget.SelectRows)
-            # configure the header so we can click on it to sort
-            header = msgWidget.header()
-            header.setSectionsClickable(1)
-            header.setSortIndicatorShown(1)
-            # show sort indicator ascending on Time, if not sorting, because we append incoming messages
-            header.setSortIndicator(0, QtCore.Qt.AscendingOrder)
-            header.myTreeWidget = msgWidget
-            header.sectionClicked.connect(msgWidget.tableHeaderClicked)
             
             # add it to the tab widget, so the user can see it
-            widgetName = msgName
-            msgRoute = Messaging.MsgRoute(msg)
-            if len(msgRoute) > 0 and not(all ("0" == a for a in msgRoute)):
-                widgetName += " ("+"->".join(msgRoute)+")"
-            self.tabWidget.addTab(msgWidget, widgetName)
-            
-            # add table header, one column for each message field
-            tableHeader = []
-            tableHeader.append("Time (ms)")
-            for fieldInfo in type(msg).fields:
-                tableHeader.append(fieldInfo.name)
-                for bitInfo in fieldInfo.bitfieldInfo:
-                    tableHeader.append(bitInfo.name)
-            
-            msgWidget.setHeaderLabels(tableHeader)
+            self.tabWidget.addTab(msgWidget, msgKey)
             
             # store a pointer to it, so we can find it next time (instead of creating it again)
-            self.msgWidgets[id] = msgWidget
+            self.msgWidgets[msgKey] = msgWidget
         
-        msgStringList = []
-        columnAlerts = []
-        try:
-            msgStringList.append(str(msg.hdr.GetTime()))
-        except AttributeError:
-            msgStringList.append("Unknown")
-        columnAlerts.append(0)
-        keyColumn = -1
-        columnCounter = 1
-        for fieldInfo in type(msg).fields:
-            if(fieldInfo.count == 1):
-                fieldValue = str(Messaging.get(msg, fieldInfo))
-                msgStringList.append(fieldValue)
-                columnAlerts.append(Messaging.getAlert(msg, fieldInfo))
-                if replaceMode and fieldInfo.name == self.keyFields[msgName]:
-                    keyValue = fieldValue
-                    keyColumn = columnCounter
-                columnCounter += 1
-                for bitInfo in fieldInfo.bitfieldInfo:
-                    fieldValue = str(Messaging.get(msg, bitInfo))
-                    msgStringList.append(fieldValue)
-                    columnAlerts.append(Messaging.getAlert(msg, bitInfo))
-                    if replaceMode and bitInfo.name == self.keyFields[msgName]:
-                        keyValue = fieldValue
-                        keyColumn = columnCounter
-                    columnCounter += 1
-            else:
-                columnText = ""
-                alert = 0
-                for i in range(0,fieldInfo.count):
-                    fieldValue = Messaging.get(msg, fieldInfo, i)
-                    # if the value is what is given when we go off the end of an array, break.
-                    if fieldInfo.type == "int" and fieldValue == "UNALLOCATED":
-                        break
-                    columnText += str(fieldValue)
-                    if Messaging.getAlert(msg, fieldInfo, i):
-                        alert = 1
-                    if(i<fieldInfo.count-1):
-                        columnText += ", "
-                msgStringList.append(columnText)
-                columnAlerts.append(alert)
-                columnCounter += 1
-
-        msgItem = TreeWidgetItem(None,msgStringList)
-        for column in range(0, len(columnAlerts)):
-            if columnAlerts[column]:
-                font = msgItem.font(column)
-                brush = msgItem.foreground(column)
-                font.setBold(1)
-                brush.setColor(QtCore.Qt.red)
-                msgItem.setFont(column, font)
-                msgItem.setForeground(column, brush)
-                msgItem.setBackground(column, brush)
-        if replaceMode and keyColumn >= 0:
-            # find row that has key field that matches ours
-            foundAndReplaced = 0
-            for i in range(0, self.msgWidgets[id].topLevelItemCount()):
-                item = self.msgWidgets[id].topLevelItem(i)
-                if item.text(keyColumn) == keyValue:
-                    foundAndReplaced = 1
-                    self.msgWidgets[id].takeTopLevelItem(i)
-                    self.msgWidgets[id].insertTopLevelItem(i, msgItem)
-            if not foundAndReplaced:
-                self.msgWidgets[id].addTopLevelItem(msgItem)
-                self.msgWidgets[id].sortItems(keyColumn, QtCore.Qt.AscendingOrder)
-        else:
-            self.msgWidgets[id].addTopLevelItem(msgItem)
-            if(self.autoscroll):
-                self.msgWidgets[id].scrollToItem(msgItem)
-            # when deleting, make a bunch of room so that if we're auto scrolling we have a bit of time before it
-            # shifts the data.  Otherwise the user can't read stuff if it's going by too fast.
-            if self.msgWidgets[id].topLevelItemCount() > MsgInspector.MAX_ROWS:
-                for i in range(0, MsgInspector.ROWS_TO_DELETE):
-                    self.msgWidgets[id].takeTopLevelItem(0)
-                    
-        if firstTime:
-            count = 0
-            for fieldInfo in type(msg).fields:
-                msgWidget.resizeColumnToContents(count)
-                count += 1
-
+        # give the data to the widget
+        self.msgWidgets[msgKey].addData(msg, self.autoscroll)
 
 # main starts here
 if __name__ == '__main__':
