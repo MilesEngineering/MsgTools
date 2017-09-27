c2d13e337bd76b6754d42a4ea6b8e874c5514958
Miles Gazic <mgazic@github.com>
2017-09-27 15:29:37 -0600

    Refactor to clean up.

--- MsgConsole/MsgConsole.py
+++ MsgConsole/MsgConsole.py
@@ -8,92 +8,78 @@ import sys
 import asyncio
 import websockets
 import janus
-import signal
+import queue
 
 loop = asyncio.get_event_loop()
 
-async def handle_tcp_client(client_reader):
-    #print("handle_tcp_client")
-    while True:
-        # we *should* read Messaging.hdr.SIZE bytes,
-        # then parse header to see body length,
-        # then read those bytes.
-        data = await client_reader.read(1024)
-        if not data:
-            break
-        await consoleserver.send_to_others(client_reader, data)
-
-def client_connected_handler(client_reader, client_writer):
-    # Start a new asyncio.Task to handle this specific client connection
-    task = asyncio.Task(handle_tcp_client(client_reader))
-    print("Added new client to list of " + str(len(consoleserver.tcp_clients)) + " clients")
-    consoleserver.tcp_clients[task] = (client_reader, client_writer)
-
-    def client_done(task):
-        print("client exited")
-        # When the tasks that handles the specific client connection is done
-        del consoleserver.tcp_clients[task]
-
-    # Add the client_done callback to be run when the future becomes done
-    task.add_done_callback(client_done)
-
-async def handle_console_input():
-    loop = asyncio.get_event_loop()
-    while True:
-        data = await consoleserver.msg_q.async_q.get()
-        await consoleserver.send_to_others(consoleserver.msg_q, data)
-
-async def handle_ws_client(websocket, path):
-    consoleserver.ws_clients[websocket] = websocket
-    print("websocket connected")
-    try:
-        while True:
-            data = await websocket.recv()
-            if not data:
-                break
-            await consoleserver.send_to_others(websocket, data)
-    except websockets.exceptions.ConnectionClosed:
-        print("websocket closed")
-        del consoleserver.ws_clients[websocket]
-
 class ConsoleServer:
+    def __init__(self):
+        from threading import Thread
+        t = Thread(target=self.start)
+        t.start()
+
+    # how to send a message, from the outside world
+    def send_message(self, data):
+        self.synchronous_tx_queue.sync_q.put(data)
+    
+    # how to get a message, from the outside world
+    def get_message(self, timeout):
+        try:
+            return self.synchronous_rx_queue.get(True, timeout)
+        except queue.Empty:
+            return None
+
+    def stop(self):
+        for task in asyncio.Task.all_tasks():
+            task.cancel()
+        #consoleserver.stop()
+        consoleserver.tcp_server.close()
+        consoleserver.loop.stop()
+        # Some thread hangs and sys.exit doesn't return, perhaps?
+        sys.exit(0)
+
+    # Implementation details from here down
     def start(self):
         # general asyncio stuff
         asyncio.set_event_loop(loop)
         self.loop = asyncio.get_event_loop()
 
-        # console input
-        self.msg_q = janus.Queue(loop=loop)
-        asyncio.ensure_future(handle_console_input())
+        # console input/output
+        self.synchronous_tx_queue = janus.Queue(loop=loop)
+        self.synchronous_rx_queue = queue.Queue()
+        asyncio.ensure_future(self.handle_console_input())
 
         # client lists
         self.tcp_clients = {} # task -> (reader, writer)
         self.ws_clients = {}
 
         # tcp server
-        self.tcp_server = self.loop.run_until_complete(asyncio.start_server(client_connected_handler, '127.0.0.1', 5678))
+        self.tcp_server = self.loop.run_until_complete(asyncio.start_server(self.client_connected_handler, '127.0.0.1', 5678))
         
         # websocket server
-        start_ws_server = websockets.serve(handle_ws_client, '127.0.0.1', 5679)
+        start_ws_server = websockets.serve(self.handle_ws_client, '127.0.0.1', 5679)
         self.loop.run_until_complete(start_ws_server)
         
         # kick things off
         self.loop.run_forever()
-        self.stop()
+        self.stopped()
 
-    def stop(self):
+    async def handle_console_input(self):
+        loop = asyncio.get_event_loop()
+        while True:
+            data = await self.synchronous_tx_queue.async_q.get()
+            await self.send_to_others(self.synchronous_tx_queue, data)
+
+    def stopped(self):
         self.tcp_server.close()
         self.loop.stop()
         self.loop.close()
     
     async def send_to_others(self, me, data):
-        if me != self.msg_q:
+        if me != self.synchronous_tx_queue:
             try:
-                # print as JSON for debug purposes
-                hdr = Messaging.hdr(data)
-                msg = Messaging.MsgFactory(hdr)
-                json = Messaging.toJson(msg)
-                print(json)
+                #print("writing to synchronous_rx_queue")
+                self.synchronous_rx_queue.put(data)
             except:
                 pass
 
@@ -102,43 +88,64 @@ class ConsoleServer:
         #        connection.sendMsg(msg)
 
         # send to Websocket clients
-        for ws in consoleserver.ws_clients.keys():
+        for ws in self.ws_clients.keys():
             if ws != me:
                 # calling ws.send REQUIRES an 'await', otherwise data never gets to the ws client!
                 await ws.send(data)
 
         # send to TCP clients
-        for task in consoleserver.tcp_clients.keys():
-                (reader, writer) = consoleserver.tcp_clients[task]
+        for task in self.tcp_clients.keys():
+                (reader, writer) = self.tcp_clients[task]
                 if reader != me:
                     writer.write(data)
 
-consoleserver = ConsoleServer()
+    async def handle_tcp_client(self, client_reader):
+        #print("handle_tcp_client")
+        while True:
+            # we *should* read Messaging.hdr.SIZE bytes,
+            # then parse header to see body length,
+            # then read those bytes.
+            data = await client_reader.read(1024)
+            if not data:
+                break
+            await self.send_to_others(client_reader, data)
 
-def background_thread_for_io():
-    consoleserver.start()
+    def client_connected_handler(self, client_reader, client_writer):
+        # Start a new asyncio.Task to handle this specific client connection
+        task = asyncio.Task(self.handle_tcp_client(client_reader))
+        print("Added new client to list of " + str(len(self.tcp_clients)) + " clients")
+        self.tcp_clients[task] = (client_reader, client_writer)
 
-def signal_handler(signal, frame):
-    print('You pressed Ctrl+C!')
-    for task in asyncio.Task.all_tasks():
-        task.cancel()
-    #consoleserver.stop()
-    consoleserver.tcp_server.close()
-    consoleserver.loop.stop()
-    sys.exit(0)
+        def client_done(task):
+            print("client exited")
+            # When the tasks that handles the specific client connection is done
+            del self.tcp_clients[task]
+
+        # Add the client_done callback to be run when the future becomes done
+        task.add_done_callback(client_done)
+
+    async def handle_ws_client(self, websocket, path):
+        self.ws_clients[websocket] = websocket
+        print("websocket connected")
+        try:
+            while True:
+                data = await websocket.recv()
+                if not data:
+                    break
+                await self.send_to_others(websocket, data)
+        except websockets.exceptions.ConnectionClosed:
+            print("websocket closed")
+            del self.ws_clients[websocket]
 
 if __name__ == "__main__":
-    signal.signal(signal.SIGINT, signal_handler)
+    # annoying stuff to start Messaging.
+    # this should be simpler!
     thisFileDir = os.path.dirname(os.path.abspath(__file__))
     sys.path.append(thisFileDir+"/../MsgApp")
     from Messaging import Messaging
-    srcroot=os.path.abspath(os.path.dirname(os.path.abspath(__file__))+"/..")
-    msgdir = srcroot+"/../obj/CodeGenerator/Python/"
-    msgLib = Messaging(msgdir, 0, "NetworkHeader")
+    msgLib = Messaging(thisFileDir+"/../../obj/CodeGenerator/Python/", 0, "NetworkHeader")
 
-    from threading import Thread
-    t = Thread(target=background_thread_for_io)
-    t.start()
+    consoleserver = ConsoleServer()
 
     _cmd = ""
     try:
@@ -146,8 +153,22 @@ if __name__ == "__main__":
             cmd = input("")
             #print("got input cmd [" + cmd + "]")
             if cmd:
-                msg = Messaging.csvToMsg(cmd)
-                if msg:
-                    consoleserver.msg_q.sync_q.put(msg.rawBuffer().raw)
-    except SystemExit:
-        print("exiting main")
+                if cmd == "getmsg":
+                    # this blocks until message received
+                    data = consoleserver.get_message(1)
+                    if data:
+                        # print as JSON for debug purposes
+                        hdr = Messaging.hdr(data)
+                        msg = Messaging.MsgFactory(hdr)
+                        json = Messaging.toJson(msg)
+                        print(json)
+                    else:
+                        print("{}")
+                else:
+                    msg = Messaging.csvToMsg(cmd)
+                    if msg:
+                        consoleserver.send_message(msg.rawBuffer().raw)
+    # I can't get exit on Ctrl-C to work!
+    except KeyboardInterrupt:
+        print('You pressed Ctrl+C!')
+        consoleserver.stop()
