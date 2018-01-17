84e5bb0f8c10efc05c7b2567a46ed8fb29c54a8b
Miles Gazic <mgazic@github.com>
2018-01-16 17:31:39 -0700

    Move Message.java from msgtools to msgplugin, to make it easier to override.

--- msgtools/parser/java/Template.java
+++ msgtools/parser/java/Template.java
@@ -16,7 +16,7 @@ import java.util.Map;
 import msgtools.FieldInfo;
 import msgtools.FieldAccess;
 
-public class <MSGSHORTNAME> extends msgtools.Message
+public class <MSGSHORTNAME> extends msgplugin.Message
 {
     public static final int MSG_ID = (int)<MSGID>;
     public static final int MSG_SIZE = <MSGSIZE>;
