69c5c0969466b19d288d31f1e15336b44307ffb6
Miles Gazic <mgazic@github.com>
2018-04-03 13:12:10 -0600

    Remove erroneously committed limit on simulation data.

--- msgtools/noisemaker/noisemaker.py
+++ msgtools/noisemaker/noisemaker.py
@@ -39,7 +39,7 @@ class NoiseMaker(msgtools.lib.gui.Gui):
         self.msgPeriod = {}
         self.msgTxTime = {}
         for msgName in Messaging.MsgClassFromName:
-            if msgName.startswith("Experimental.Accel"):
+            if not msgName.startswith("Network"):
                 #print("found message " + msgName)
                 self.msgPeriod[msgName] = period
                 period = period + 0.020
