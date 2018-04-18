36f5c15881d266a41b13946f3002c70f1fe8b678
Miles Gazic <mgazic@github.com>
2018-04-18 10:34:02 -0600

    Update version for new release.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='msgtools',
-    version='0.28.40',
+    version='0.28.42',
     description='Tools for fixed binary protocols',
     url='https://github.com/MilesEngineering/MsgTools/',
     author='Miles Gazic',
