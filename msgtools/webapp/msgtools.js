32193617a78c41e63147af8edd18e86897a66893
mosminerCP <mosminer@cardinalpeak.com>
2018-05-16 11:42:50 -0600

    Add prettyPrint function

--- msgtools/webapp/msgtools.js
+++ msgtools/webapp/msgtools.js
@@ -636,6 +636,37 @@
         return {websocketServer: ws, websocketPort:port}    
     }
 
+    // Make a pretty log output.  This is also an example of how to iterate over 
+    // the fields of a message, getting metadata about each field (and bitfield), 
+    // and getting the value of the field using the get function called with 
+    // bracket notation obj[fnName](), instead of obj.fn()
+    function prettyPrint(obj) {
+        var ret = "Reflection information obtained by iterating over list of fields\n";
+        var proto = Object.getPrototypeOf(obj);
+        for(var i=0; i<proto.fields.length; i++)
+        {
+            var field = proto.fields[i];
+            var numBitfields = field.bitfieldInfo.length;
+            if(numBitfields == 0)
+            {
+                ret += "name: " + field.name;
+                if(field.count != 1)
+                    ret += "[" + field.count + "]";
+                ret +=", type="+field.type+ ", value="+obj[field.get]()+", " + "desc=" + field.description + ", range=("+field.minVal + ","+field.maxVal+")"+ "\n";
+            }
+            else
+            {
+                for(var j=0; j<field.bitfieldInfo.length; j++)
+                {
+                    bitfield = field.bitfieldInfo[j];
+                    ret += "name: " + field.name+"."+bitfield.name;
+                    ret += ", type="+bitfield.type+", value="+obj[bitfield.get]()+", " + "desc=" + bitfield.description + ", range=("+bitfield.minVal + ","+bitfield.maxVal+")"+ "\n";
+                }
+            }
+        }
+        return ret;
+    }
+
     // Module Exports...
     return {
         setMsgDirectory : setMsgDirectory,
@@ -645,6 +676,7 @@
         UnknownMsg : UnknownMsg,
         toJSON : toJSON,
         buf2hex : buf2hex,
-        getWebsocketURLParams : getWebsocketURLParams
+        getWebsocketURLParams : getWebsocketURLParams,
+        prettyPrint : prettyPrint
     }
 })()
