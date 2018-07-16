70a70802da7988aeafde7ad6d9ac541a2122b6fc
Miles Gazic <mgazic@github.com>
2018-07-15 20:21:50 -0700

    More progress on accessors.

--- msgtools/parser/jinja/ctemplate.h
+++ msgtools/parser/jinja/ctemplate.h
@@ -26,19 +26,13 @@ double
 <ENUMERATIONS>
 
 #for f in msg.Fields:
-    #if f.Default is defined:
-{{'#'}}define {{globals.msgname}}_{{f.Name}}_DEFAULT {{f.Default}}
-    #endif
-    #if f.Count is defined:
-{{'#'}}define {{globals.msgname}}_{{f.Name}}_COUNT {{f.Count}}
-    #endif
-{{'#'}}define {{globals.msgname}}_{{f.Name}}_OFFSET {{ f.Location }}
-{{'#'}}define {{globals.msgname}}_{{f.Name}}_SIZE {{msgutils.fieldSize(f)}}
-    # for bf in f.Bitfields:
-        # if bf.Default is defined:
-{{'#'}}define {{globals.msgname}}_{{bf.Name}}_DEFAULT {{bf.Default}}
-        #endif
-    #endfor
+{{"#define "~globals.msgname~"_"~f.Name~"_DEFAULT "~f.Default~"\n" if f.Default is defined-}}
+{{"#define "~globals.msgname~"_"~f.Name~"_COUNT "~f.Count~"\n" if f.Count is defined-}}
+{{"#define "~globals.msgname~"_"~f.Name~"_OFFSET "~f.Location }}
+{{"#define "~globals.msgname~"_"~f.Name~"_SIZE "~msgutils.fieldSize(f)}}
+{%for bf in f.Bitfields%}
+{{"#define "~globals.msgname~"_"~bf.Name~"_DEFAULT "~bf.Default~"\n" if bf.Default is defined -}}
+{%endfor%}
 #endfor
 
 
@@ -46,13 +40,28 @@ double
 /*  {{f.Units}}, ({{msgutils.fieldMin(f)}} to {{msgutils.fieldMax(f)}})*/
 INLINE {{fieldType(f)}} {{globals.msgname}}_Get{{f.Name}}(uint8_t* m_data{%if msgutils.fieldCount(f) > 1%}, int idx{%endif%})
 {
-    return Get_{{fieldType(f)}}(&m_data[{{f.Location}}{%if msgutils.fieldCount(f) > 1%}+idx*{{msgutils.fieldSize(f)}}{%endif%}]);
+    {% set access %}Get_{{fieldType(f)}}(&m_data[{{f.Location}}{%if msgutils.fieldCount(f) > 1%}+idx*{{msgutils.fieldSize(f)}}{%endif%}]){% endset %}
+    return {{ msgutils.getMath(access, f, msgutils.typeForScaledInt(f), 'f') }};
 }
     #for bf in f.Bitfields:
 /*  {{bf.Units}}, ({{msgutils.fieldMin(bf)}} to {{msgutils.fieldMax(bf)}})*/
-INLINE uint32_t {{globals.msgname}}_Get{{bf.Name}}(uint8_t* m_data)
+INLINE {{fieldType(f)}} {{globals.msgname}}_Get{{bf.Name}}(uint8_t* m_data)
 {
-    return ({{globals.msgname}}_Get{{f.Name}}(m_data) >> 0) & 0x7fffffff;
+    return ({{globals.msgname}}_Get{{f.Name}}(m_data) >> {{bf.BitLocation}}) & {{msgutils.Mask(bf.NumBits)}};
+}
+    #endfor
+#endfor
+#for f in msg.Fields:
+/*  {{f.Units}}, ({{msgutils.fieldMin(f)}} to {{msgutils.fieldMax(f)}})*/
+INLINE void {{globals.msgname}}_Set{{f.Name}}(uint8_t* m_data, {{fieldType(f)}} value{%if msgutils.fieldCount(f) > 1%}, int idx{%endif%})
+{
+    Set_{{fieldType(f)}}(&m_data[{{f.Location}}{%if msgutils.fieldCount(f) > 1%}+idx*{{msgutils.fieldSize(f)}}{%endif%}], value);
+}
+    #for bf in f.Bitfields:
+/*  {{bf.Units}}, ({{msgutils.fieldMin(bf)}} to {{msgutils.fieldMax(bf)}})*/
+INLINE void {{globals.msgname}}_Set{{bf.Name}}(uint8_t* m_data, {{fieldType(f)}} value)
+{
+    {{globals.msgname}}_Set{{f.Name}}(m_data, ({{globals.msgname}}_Get{{f.Name}}(m_data) & ~({{msgutils.Mask(bf.NumBits)}} << {{bf.BitLocation}})) | ((value & {{msgutils.Mask(bf.NumBits)}}) << {{bf.BitLocation}}));
 }
     #endfor
 #endfor
