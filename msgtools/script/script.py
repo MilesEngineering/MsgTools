0139fbec77710cf04591cc33d25f465642d15cd0
Miles Gazic <mgazic@github.com>
2018-10-13 21:41:24 -0600

    Split EditWindow into separate file. Change imports to handle however we're invoked.

--- msgtools/script/script.py
+++ msgtools/script/script.py
@@ -1,16 +1,16 @@
-import sys, os
-
+import sys, os, multiprocessing
 from PyQt5 import QtWidgets, QtCore, QtGui
-from PyQt5.Qsci import QsciScintilla, QsciLexerPython
-import multiprocessing
 
-
+# Need two styles of import, depending on if we were launched from an installed package,
+# or just from running our script via 'python script.py'
 try:
-    from msgtools.script import debugger
-except ImportError:
-    srcroot=os.path.abspath(os.path.dirname(os.path.abspath(__file__))+"/../../..")
-    sys.path.append(srcroot)
-    from msgtools.script import debugger
+    from .edit_window import EditWindow
+    from ..script import debugger as debugger
+    print('1')
+except:
+    from edit_window import EditWindow
+    import debugger
+    print('2')
 
 NEW_FILE = '''\
 from msgtools.lib.messaging import Messaging as M
@@ -20,91 +20,6 @@ from msgtools.console.client import Client
 M.LoadAllMessages()
 cxn = Client('example')
 '''
-
-class SimplePythonEditor(QsciScintilla):
-    DEBUG_MARKER_NUM = 8
-    EXEC_MARKER_NUM = 9
-
-    def __init__(self, parent=None):
-        super(SimplePythonEditor, self).__init__(parent)
-        
-        # Set the default font
-        font = QtGui.QFont()
-        font.setFamily('Courier')
-        font.setFixedPitch(True)
-        font.setPointSize(10)
-        self.setFont(font)
-        self.setMarginsFont(font)
-
-        # Margin 0 is used for line numbers
-        fontmetrics = QtGui.QFontMetrics(font)
-        self.setMarginsFont(font)
-        self.setMarginWidth(0, fontmetrics.width("00000") + 6)
-        self.setMarginLineNumbers(0, True)
-        self.setMarginsBackgroundColor(QtGui.QColor("#cccccc"))
-
-        self.setIndentationsUseTabs(False)
-        self.setIndentationWidth(4)
-
-        # Clickable margin 1 for showing markers
-        self.setMarginSensitivity(1, True)
-        self.marginClicked.connect(self.on_margin_clicked)
-        
-        # debug marker
-        self.markerDefine(QsciScintilla.Circle, self.DEBUG_MARKER_NUM)
-        self.setMarkerBackgroundColor(QtGui.QColor("#1111ee"), self.DEBUG_MARKER_NUM)
-
-        # execut emarker
-        self.markerDefine(QsciScintilla.Circle, self.EXEC_MARKER_NUM)
-        self.setMarkerBackgroundColor(QtGui.QColor("#11ee11"), self.EXEC_MARKER_NUM)
-
-        # Brace matching: enable for a brace immediately before or after
-        # the current position
-        #
-        self.setBraceMatching(QsciScintilla.SloppyBraceMatch)
-
-        # Current line visible with special background color
-        self.setCaretLineVisible(True)
-        self.setCaretLineBackgroundColor(QtGui.QColor("#ffe4e4"))
-
-        # Set Python lexer
-        lexer = QsciLexerPython()
-        lexer.setDefaultFont(font)
-        self.setLexer(lexer)
-
-        text = bytearray(str.encode("Arial"))
-        self.SendScintilla(QsciScintilla.SCI_STYLESETFONT, 1, text)
-
-        # not too small
-        self.setMinimumSize(600, 450)
-        
-        self.last_exec_line = 0
-
-    def on_margin_clicked(self, nmargin, nline, modifiers):
-        # Toggle debug marker for the line the margin was clicked on
-        if self.markersAtLine(nline) != 0:
-            self.markerDelete(nline, self.DEBUG_MARKER_NUM)
-        else:
-            self.markerAdd(nline, self.DEBUG_MARKER_NUM)
-
-    # function called to indicate we executed up to a line of code
-    def ran_to_line(self, nline):
-        # change line number to zero based indexing
-        nline = nline - 1
-        if self.markersAtLine(self.last_exec_line) != 0:
-            self.markerDelete(self.last_exec_line, self.EXEC_MARKER_NUM)
-        if nline >= 0:
-            self.markerAdd(nline, self.EXEC_MARKER_NUM)
-        self.last_exec_line = nline
-    
-    def has_breakpoint(self, nline):
-        # change line number to zero based indexing
-        nline = nline - 1
-        markerBitmask = self.markersAtLine(self.last_exec_line)
-        if markerBitmask:
-            if (1 << self.DEBUG_MARKER_NUM) & markerBitmask:
-                return True
-        return False
 
 class MsgScript(QtWidgets.QMainWindow):
     TextOutput = QtCore.pyqtSignal(object)
@@ -177,7 +92,7 @@ class MsgScript(QtWidgets.QMainWindow):
         self.setWindowTitle("MsgScript")
 
         # editor/debug window
-        self.editor = SimplePythonEditor()
+        self.editor = EditWindow()
         self.editor.modificationChanged.connect(self.document_was_modified)
         self.editor.setText("")
         self.current_filename = ""
