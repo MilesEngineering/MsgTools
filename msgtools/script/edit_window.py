ef56b503ccf491a80af4c14ff7770a77fe2d6be4
Miles Gazic <mgazic@github.com>
2018-10-14 13:04:51 -0600

    Have a separate function for exited vs. crashed.  Currently both use crash marker. Delete the crash marker twice in ran_to_line.  Not sure why this is necessary.

--- msgtools/script/edit_window.py
+++ msgtools/script/edit_window.py
@@ -2,9 +2,9 @@ from PyQt5 import QtGui
 from PyQt5.Qsci import QsciScintilla, QsciLexerPython
 
 class EditWindow(QsciScintilla):
+    CRASH_MARKER_NUM = 7
     DEBUG_MARKER_NUM = 8
     EXEC_MARKER_NUM = 9
-    CRASH_MARKER_NUM = 10
 
     def __init__(self, parent=None):
         super(EditWindow, self).__init__(parent)
@@ -71,27 +71,38 @@ class EditWindow(QsciScintilla):
             self.markerDelete(nline, self.DEBUG_MARKER_NUM)
         else:
             self.markerAdd(nline, self.DEBUG_MARKER_NUM)
-    
+            
+    def exited(self):
+        markerBitmask = self.markersAtLine(self.last_exec_line)
+        if (1 << self.EXEC_MARKER_NUM) & markerBitmask:
+            self.markerDelete(self.last_exec_line, self.EXEC_MARKER_NUM)
+        self.markerAdd(self.last_exec_line, self.CRASH_MARKER_NUM)
+
     def crashed(self):
-        self.markerDelete(self.last_exec_line, self.EXEC_MARKER_NUM)
+        markerBitmask = self.markersAtLine(self.last_exec_line)
+        if (1 << self.EXEC_MARKER_NUM) & markerBitmask:
+            self.markerDelete(self.last_exec_line, self.EXEC_MARKER_NUM)
         self.markerAdd(self.last_exec_line, self.CRASH_MARKER_NUM)
 
     # function called to indicate we executed up to a line of code
     def ran_to_line(self, nline):
         # change line number to zero based indexing
         nline = nline - 1
-        if self.markersAtLine(self.last_exec_line) != 0:
+        markerBitmask = self.markersAtLine(self.last_exec_line)
+        if (1 << self.EXEC_MARKER_NUM) & markerBitmask:
             self.markerDelete(self.last_exec_line, self.EXEC_MARKER_NUM)
+        if (1 << self.CRASH_MARKER_NUM) & markerBitmask:
+            # We have to delete crash marker twice!  Dunno why!
+            self.markerDelete(self.last_exec_line, self.CRASH_MARKER_NUM)
             self.markerDelete(self.last_exec_line, self.CRASH_MARKER_NUM)
         if nline >= 0:
             self.markerAdd(nline, self.EXEC_MARKER_NUM)
-        self.last_exec_line = nline
+            self.last_exec_line = nline
     
     def has_breakpoint(self, nline):
         # change line number to zero based indexing
         nline = nline - 1
         markerBitmask = self.markersAtLine(self.last_exec_line)
-        if markerBitmask:
-            if (1 << self.DEBUG_MARKER_NUM) & markerBitmask:
-                return True
+        if (1 << self.DEBUG_MARKER_NUM) & markerBitmask:
+            return True
         return False
