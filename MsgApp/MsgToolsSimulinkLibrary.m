03c3e9f43db2292db669881aba4a39e81966f149
Miles Gazic <mgazic@github.com>
2021-02-26 18:56:38 -0700

    Set build options on library blocks.  Generate a .h file that #includes all generated code, for use by simulink.

--- MsgApp/MsgToolsSimulinkLibrary.m
+++ MsgApp/MsgToolsSimulinkLibrary.m
@@ -1,6 +1,7 @@
 classdef MsgToolsSimulinkLibrary < handle
     properties
         position = [10 10];
+        header_file_contents = [];
     end
     properties (Constant)
         header_params = ["Destination", "Source"];
@@ -11,20 +12,58 @@ classdef MsgToolsSimulinkLibrary < handle
         function obj = MsgToolsSimulinkLibrary(simulink_block_library_name, msgdir)
             fprintf("Creating Simulink block library %s\n", simulink_block_library_name);
             if(nargin < 2)
-                msgdir = '../../obj/CodeGenerator/Simulink/';
+                msgdir = '../../obj/CodeGenerator';
                 fprintf('msgdir: %s\n', msgdir);
             end
+            cdir = msgdir + '/C';
+            msgdir = msgdir + '/Simulink';
+            obj.header_file_contents = [
+                "#include <stdint.h>"
+                "#include ""CFieldAccess.h"""
+                "#include ""simulink_message_interface.h"""
+            ];
             
             % create the library
-            new_system(simulink_block_library_name, "Library");
+            lib = new_system(simulink_block_library_name, "Library");
             open_system(simulink_block_library_name);
+            set_param(lib, "SimTargetLang", "C++");
+            %set_param(lib, "SimUseLocalCustomCode", "on"); % defaults to 'on'
+            %set_param(lib, "SimCustomSourceCode", "");%Simulation Custom Code/Insert custom C code in generated: Source file
+            set_param(lib, "SimCustomHeaderCode", '#include "msgtools.h"');%Simulation Custom Code/Insert custom C code in generated: Header file
+            %set_param(lib, "SimCustomInitializer", "");%Simulation Custom Code/Insert custom C code in generated: Initialize function
+            %set_param(lib, "SimCustomTerminator", "");%Simulation Custom Code/Insert custom C code in generated: Terminate function
+            set_param(lib, "SimUserIncludeDirs", "../obj/CodeGenerator");%Simulation Custom Code/Additional build information: Include directories
+            %set_param(lib, "SimUserSources", "");%Simulation Custom Code/Additional build information: Source Files
+            set_param(lib, "SimUserLibraries", "../obj/simulink/msgtools.a");%Simulation Custom Code/Additional build information: Libraries
+            %set_param(lib, "SimUserDefines", "");%Simulation Custom Code/Additional build information: Defines
+
+            set_param(lib, "RTWUseSimCustomCode", "on"); % Code Generation Custom Code / Use the same custom code settings as Simulation Target
+            % RTWUseLocalCustomCode: 'on'/'off' Use local custom code settings(do not inherit from main model)
+            % below params aren't needed if RTWUseSimCustomCode is 'on'
+            %Configuration Parameters / Code Generation Custom Code
+            %  CustomSourceCode: Insert custom C code in generated: Source file
+            %  CustomHeaderCode: Insert custom C code in generated: Header file
+            % CustomInitializer: Insert custom C code in generated: Initialize function
+            %  CustomTerminator: Insert custom C code in generated: Terminate function
+            %     CustomInclude: Additional build information: Include directories
+            %      CustomSource: Additional build information: Source files
+            %     CustomLibrary: Additional build information: Libraries
+            %      CustomDefine: Additional build information: Defines
+
+
 
             %execute all the generated matlab files that create simulink blocks!
             addpath(MsgToolsSimulinkLibrary.AbsPath(msgdir));
             obj.ProcessDir(simulink_block_library_name, msgdir, msgdir);
+            obj.ProcessHeaderDir(cdir, cdir);
 
             % save the model
             save_system(simulink_block_library_name);
+            
+            % write the header file
+            fileID = fopen('msgtools.h','w');
+            fprintf(fileID, '%s\n',obj.header_file_contents{:});
+            fclose(fileID);
         end
 
         function ProcessDir(obj, simulink_block_library_name, basedir, dirname)
@@ -44,7 +83,7 @@ classdef MsgToolsSimulinkLibrary < handle
 
                 filename = filenames(f).name;
                 path = fullfile(dirname, filename);
-                fprintf("Running %s\n", path)
+                fprintf("Running %s\n", path);
                 [inputblk, outputblk] = feval(functionname, simulink_block_library_name, MsgToolsSimulinkLibrary.header_params);
                 set_param(inputblk,'Position',obj.next_position());
                 set_param(outputblk,'Position',obj.next_position());
@@ -55,9 +94,27 @@ classdef MsgToolsSimulinkLibrary < handle
             isub = [d(:).isdir]; % returns logical vector
             subFolders = {d(isub).name}';
             subFolders(ismember(subFolders,{'.','..','+headers'})) = [];
-            % Print folder names to command window.
+            % recurse into subdir.
             for k = 1 : length(subFolders)
                 obj.ProcessDir(simulink_block_library_name, basedir, char(strcat(dirname,'/',subFolders(k))));
+            end
+        end
+        function ProcessHeaderDir(obj, basedir, dirname)
+            fprintf('Processing %s\n', dirname);
+            filenames = dir(strcat(dirname,'/*.h'));
+            % loop over filenames in dir
+            for f = 1:numel(filenames)
+                filename = filenames(f).name;
+                obj.header_file_contents(end+1) = "#include """ + dirname + "/" + filename + """";
+            end
+            % Get a list of all files and folders in this folder.
+            d = dir(dirname);
+            isub = [d(:).isdir]; % returns logical vector
+            subFolders = {d(isub).name}';
+            subFolders(ismember(subFolders,{'.','..'})) = [];
+            % recurse into subdir.
+            for k = 1 : length(subFolders)
+                obj.ProcessHeaderDir(basedir, char(strcat(dirname,'/',subFolders(k))));
             end
         end
         function p = next_position(obj)
