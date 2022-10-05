f8c5d69f1b405e112524e493f192bb01aa0a985e
Miles Gazic <mgazic@github.com>
2022-10-05 12:57:50 +0100

    Add new versions of reading a binary file, with only one of them enabled.  Leave the others in as footnotes for future optimzation work.

--- msgtools/lib/file_reader.py
+++ msgtools/lib/file_reader.py
@@ -1,5 +1,6 @@
 from msgtools.lib.messaging import Messaging
 from msgtools.lib.header_translator import HeaderHelper
+import ctypes
 import math
 
 class MessageFileReader:
@@ -26,16 +27,43 @@ class MessageFileReader:
 
         with open(filename, mode='rb') as f:
             while(1):
-                msg_bytes = f.read(Messaging.hdr.SIZE)
-                if len(msg_bytes) < Messaging.hdr.SIZE:
-                    break
-                hdr = Messaging.hdr(msg_bytes)
+                # Below are three versions of accomplishing the same thing.
+                # Each is faster but perhaps less straightforward than the previous.
+                # The last one is about 10% faster than the first one.
+                # For the purpose of constructing Pandas DataFrames, all methods are
+                # slower than reading a JSON file
+                if 0: # <- read bytes, create a header
+                    msg_bytes = f.read(Messaging.hdr.SIZE)
+                    if len(msg_bytes) < Messaging.hdr.SIZE:
+                        break
+                    hdr = Messaging.hdr(msg_bytes)
+                elif 0: # <- read into a header object
+                    last_header_pos = f.tell()
+                    hdr = Messaging.hdr()
+                    length_read = f.readinto(hdr.rawBuffer())
+                    if length_read < Messaging.hdr.SIZE:
+                        break
+                else: # <- read a kB of data so we do fewer (but larger) reads
+                    last_header_pos = f.tell()
+                    msg_bytes = ctypes.create_string_buffer(Messaging.hdr.SIZE+1024)
+                    length_read = f.readinto(msg_bytes)
+                    #print("%d %d" % (last_header_pos, length_read))
+                    hdr = Messaging.hdr(msg_bytes)
+                    if length_read < Messaging.hdr.SIZE:
+                        break
 
                 if not self.header_helper.header_valid(hdr):
                     print("Invalid header!")
                     break
 
-                msg_bytes += f.read(hdr.GetDataLength())
+                if 0: # <- read more data for the body
+                    msg_bytes += f.read(hdr.GetDataLength())
+                elif 0: # <- seek back to before we read the header, make a new buffer, and read into it
+                    f.seek(last_header_pos)
+                    msg_bytes = ctypes.create_string_buffer(Messaging.hdr.SIZE+hdr.GetDataLength())
+                    f.readinto(msg_bytes)
+                else: # <- seek back to just after the last message's data (where the next header starts)
+                    f.seek(last_header_pos+Messaging.hdr.SIZE+hdr.GetDataLength())
                 hdr = Messaging.hdr(msg_bytes)
                 msg = Messaging.MsgFactory(hdr)
                 #if not self.header_helper.body_valid(hdr, msg.rawBuffer()[Messaging.hdr.SIZE:]):
@@ -43,9 +71,10 @@ class MessageFileReader:
                     print("Invalid body!")
                     break
 
-                self.setup_message(msg)
+                timestamp = self.timestamp(msg)
+                self.process_message(msg, timestamp)
 
-    def setup_message(self, msg):
+    def timestamp(self, msg):
         try:
             # \todo Detect time rolling.  this only matters when we're processing a log file
             # with insufficient timestamp size, such that time rolls over from a large number
@@ -66,5 +95,4 @@ class MessageFileReader:
                 timestamp = timestamp / 1000.0
         except AttributeError:
             timestamp = 0.0
-
-        self.process_message(msg, timestamp)
+        return timestamp
