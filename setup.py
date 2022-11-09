66f3bffe27ebe70a71c5a1b2d3f350b24de8ab3e
Miles Gazic <mgazic@github.com>
2022-11-09 10:08:24 -0700

    Update version for new release.  Add dependency on gevent, for console/client.py.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(name='msgtools',
     python_requires='>=3.5',
-    version='0.36.4',
+    version='0.36.5',
     description='Tools for fixed binary protocols',
     url='https://github.com/MilesEngineering/MsgTools/',
     author='Miles Gazic',
@@ -72,7 +72,8 @@ setup(name='msgtools',
         'jinja2',
         'numpy',
         "python-can",
-        "pandas"
+        "pandas",
+        "gevent"
     ],
     extras_require={
         'gui':  ["pyqtgraph", "pyqt5", 'qscintilla'],
